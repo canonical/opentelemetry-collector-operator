27d5c52b700b495e894ee1f807e97a4eb02e1b8a
Michael Thamm <mike.thamm@canonical.com>
2025-07-22 16:06:23 -0400

    chore: Fix is_pattern_in_logs

--- tests/integration/helpers.py
+++ tests/integration/helpers.py
@@ -4,9 +4,9 @@ import jubilant
 from tenacity import retry, stop_after_attempt, wait_fixed
 
 
-@retry(stop=stop_after_attempt(15), wait=wait_fixed(10))
+@retry(stop=stop_after_attempt(3), wait=wait_fixed(10))
 async def is_pattern_in_logs(juju: jubilant.Juju, pattern: str):
     otelcol_logs = juju.ssh("otelcol/0", command="sudo snap logs opentelemetry-collector -n=all")
-    if re.search(pattern, otelcol_logs):
+    if not re.search(pattern, otelcol_logs):
         raise Exception(f"Pattern {pattern} found in the otelcol logs")
     return True
