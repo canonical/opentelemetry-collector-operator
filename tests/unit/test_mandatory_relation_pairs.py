c1c3f2aad8bb04d9839516638d591a63f98ea7bd
Michael Thamm <mike.thamm@canonical.com>
2025-07-22 15:43:12 -0400

    chore: Fix utest

--- tests/unit/test_mandatory_relation_pairs.py
+++ tests/unit/test_mandatory_relation_pairs.py
@@ -3,7 +3,7 @@ from scenario import ActiveStatus, BlockedStatus
 
 
 def test_missing_relation_pair_status(ctx):
-    source_relation = Relation("metrics-endpoint")
+    source_relation = Relation("cos-agent")
     # GIVEN the charm has no relations
     state = State(
         leader=True,
@@ -14,11 +14,11 @@ def test_missing_relation_pair_status(ctx):
     # THEN the charm enters BlockedStatus
     assert isinstance(state_out.unit_status, BlockedStatus)
     # AND the status message warns of the missing sink relations
-    assert "] for metrics-endpoint" in state_out.unit_status.message
+    assert "] for cos-agent" in state_out.unit_status.message
 
 
 def test_valid_relation_pair_status(ctx):
-    source_relation = Relation("metrics-endpoint")
+    source_relation = Relation("cos-agent")
     sink_relation = Relation("send-remote-write")
     # GIVEN the charm has a source and no sink relation
     state = State(
