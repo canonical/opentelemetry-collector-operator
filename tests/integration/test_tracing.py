0420adfb8a02119410be4cb56175e3c31ba269a6
Michael Thamm <mike.thamm@canonical.com>
2025-08-27 14:58:59 -0400

    chore: cleanup

--- tests/integration/test_tracing.py
+++ tests/integration/test_tracing.py
@@ -22,43 +22,13 @@ async def test_deploy(juju: jubilant.Juju, charm_22_04: str):
     juju.wait(
         lambda status: jubilant.all_blocked(status, "otelcol"),
         error=jubilant.any_error,
-        timeout=360,
+        timeout=240,
     )
     juju.wait(
         lambda status: jubilant.all_active(status, "postgresql"),
         error=jubilant.any_error,
-        timeout=600,
+        timeout=240,
     )
-
-
-async def test_metrics_are_scraped(juju: jubilant.Juju):
-    grep_filters = ["juju_application=postgresql", f"juju_model={juju.model}"]
-    result = await is_pattern_in_snap_logs(juju, grep_filters)
-    assert result
-
-
-# async def test_logs_are_scraped(juju: jubilant.Juju):
-#     grep_filters = ["log.file.name=zookeeper.log", "log.file.path=/snap/opentelemetry-collector"]
-#     result = await is_pattern_in_snap_logs(juju, grep_filters)
-#     assert result
-
-
-@retry(stop=stop_after_attempt(25), wait=wait_fixed(10))
-def test_alerts_are_aggregated(juju: jubilant.Juju):
-    alert_files = juju.ssh(
-        "otelcol/0",
-        command="find /var/lib/juju/agents/unit-otelcol-0/charm/prometheus_alert_rules -type f",
-    )
-    assert "postgresql" in alert_files
-
-
-# @retry(stop=stop_after_attempt(25), wait=wait_fixed(10))
-# def test_dashboards_are_aggregated(juju: jubilant.Juju):
-#     dashboard_files = juju.ssh(
-#         "otelcol/0",
-#         command="find /var/lib/juju/agents/unit-otelcol-0/charm/grafana_dashboards -type f",
-#     )
-#     assert "postgresql" in dashboard_files
 
 
 async def test_traces_are_scraped(juju: jubilant.Juju):
