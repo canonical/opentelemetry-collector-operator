6c1a52a8ced5360b27447169665cac688efaf811
Pietro Pasotti <pietro.pasotti@canonical.com>
2025-08-27 11:25:18 +0200

    bumped snap versions

--- src/snap_management.py
+++ src/snap_management.py
@@ -42,8 +42,8 @@ class SnapMap:
     snap_maps = {
         "opentelemetry-collector": {
             # (confinement, arch): revision
-            ("strict", "amd64"): 23,  # 0.130.0
-            ("strict", "arm64"): 24,  # 0.130.0
+            ("strict", "amd64"): 25,  # 0.130.0
+            ("strict", "arm64"): 26,  # 0.130.0
         },
         "node-exporter": {
             # (confinement, arch): revision
