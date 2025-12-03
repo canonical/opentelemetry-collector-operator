049dc04e23207bcf08c36266ee7197aebf5124b5
Michael Thamm <mike.thamm@canonical.com>
2025-12-03 17:04:18 -0500

    chore

--- src/config_builder.py
+++ src/config_builder.py
@@ -281,7 +281,7 @@ class ConfigBuilder:
             self.add_component(
                 Component.exporter,
                 f"debug/{self._unit_name}",
-                {"verbosity": "basic", "use_internal_logger": False},
+                {"verbosity": "normal", "use_internal_logger": False},
             )
 
     def _add_tls_to_all_receivers(
