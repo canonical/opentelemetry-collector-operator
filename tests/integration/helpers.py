3cce7064ba70cb116ef55420ff47b331a60a41f0
Michael Thamm <mike.thamm@canonical.com>
2025-12-03 17:26:04 -0500

    chore: more robust tests

--- tests/integration/helpers.py
+++ tests/integration/helpers.py
@@ -13,7 +13,7 @@ from tenacity import retry, stop_after_attempt, wait_fixed
 PATH_EXCLUDE: Final[str] = "/var/log/**/{cloud-init-output.log,syslog,auth.log};/var/log/juju/**"
 
 
-@retry(stop=stop_after_attempt(10), wait=wait_fixed(10))
+@retry(stop=stop_after_attempt(20), wait=wait_fixed(10))
 async def is_pattern_in_debug_logs(juju: jubilant.Juju, grep_filters: list):
     cmd = (
         "sudo snap logs opentelemetry-collector -n=all"
@@ -27,7 +27,6 @@ async def is_pattern_in_debug_logs(juju: jubilant.Juju, grep_filters: list):
     return True
 
 
-@retry(stop=stop_after_attempt(10), wait=wait_fixed(10))
 async def is_pattern_not_in_debug_logs(juju: jubilant.Juju, pattern: str):
     debug_logs = juju.ssh("otelcol/0", command="sudo snap logs opentelemetry-collector -n=all")
     if re.search(pattern, debug_logs):
