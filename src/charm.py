2b829234dc6da0c5bdb2dc7c6dbfb84e810c96b2
Sina P <55766091+sinapah@users.noreply.github.com>
2025-12-15 15:37:20 -0500

    fix: use machine id for receiver (#162)
    * fix: use machine id for receiver
    
    * fix: unit tests
    
    * chore: check receiver name in itest
    
    * chore: update tests
    
    * chore: add hostname as suffix to otlp receiver name
    
    * chore: fetch libs
    
    * fix: otlp receiver name
    
    * fix: remove unit test fixture

--- src/charm.py
+++ src/charm.py
@@ -204,6 +204,7 @@ class OpenTelemetryCollectorCharm(ops.CharmBase):
         # Create the config manager
         config_manager = ConfigManager(
             unit_name=self.unit.name,
+            hostname=socket.gethostname(),
             global_scrape_interval=global_configs["global_scrape_interval"],
             global_scrape_timeout=global_configs["global_scrape_timeout"],
             receiver_tls=is_tls_ready(),
@@ -451,7 +452,7 @@ class OpenTelemetryCollectorCharm(ops.CharmBase):
 
         # If the config file or any cert has changed, a change in the hash
         # will trigger a restart
-        hash_file = LocalPath("/opt/otelcol_reload")
+        hash_file = self.charm_dir.absolute()/"config_hash"
         old_hash = ""
         if hash_file.exists():
             old_hash = hash_file.read_text()
