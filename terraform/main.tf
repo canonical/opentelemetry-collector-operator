4113c4b442c235ab21e3d1609ce71e4f40311976
Michael Thamm <mike.thamm@canonical.com>
2026-05-26 09:36:13 -0400

    chore

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,7 +1,6 @@
 resource "juju_application" "opentelemetry_collector" {
   name               = var.app_name
   config             = var.config
-  constraints        = var.constraints
   model_uuid         = var.model_uuid
   storage_directives = var.storage_directives
   trust              = true # We always need this variable to be true in order to be able to apply resources limits.
