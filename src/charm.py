5f5d05befa4652b1f4ec6d12073b0ec31f206d0b
Sina <sina.pahlavan@canonical.com>
2026-08-12 16:22:59 -0400

    feat: blocked on invalid alert rules over prometheus_scrape

--- src/charm.py
+++ src/charm.py
@@ -16,6 +16,7 @@ from typing import Any, Dict, List, Mapping, Optional, cast
 import ops
 from charmlibs.pathops import LocalPath
 from charms.grafana_agent.v0.cos_agent import COSAgentRequirer
+from charms.prometheus_k8s.v0.prometheus_scrape import MetricsEndpointConsumer
 from charms.operator_libs_linux.v1.systemd import service_start
 from charms.operator_libs_linux.v2 import snap  # type: ignore
 from cosl import JujuTopology, MandatoryRelationPairs
@@ -168,6 +169,8 @@ def _get_missing_mandatory_relations(charm: CharmBase) -> Optional[str]:
 
 class OpenTelemetryCollectorCharm(ops.CharmBase):
     """Charm the service."""
+
+    metrics_consumer: MetricsEndpointConsumer
 
     def __init__(self, framework: ops.Framework):
         super().__init__(framework)
@@ -599,6 +602,10 @@ class OpenTelemetryCollectorCharm(ops.CharmBase):
         if missing_relations := _get_missing_mandatory_relations(self):
             self.unit.status = BlockedStatus(missing_relations)
 
+        # Invalid alert rules
+        if self._has_invalid_prometheus_alerts():
+            self.unit.status = BlockedStatus("Invalid Prometheus alerts. See debug-log")
+
         # Workload version
         self.unit.set_workload_version(self._otelcol_version or "")
 
@@ -907,6 +914,10 @@ class OpenTelemetryCollectorCharm(ops.CharmBase):
     @property
     def _has_server_cert_relation(self) -> bool:
         return any(self.model.relations.get("receive-server-cert", []))
+
+    def _has_invalid_prometheus_alerts(self) -> bool:
+        """Check if any metrics-endpoint relation reported invalid alert rules."""
+        return self.metrics_consumer.has_invalid_alert_rules()
 
     @property
     def _node_exporter_info_metric_file_path(self) -> LocalPath:
